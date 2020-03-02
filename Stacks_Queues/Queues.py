bfddd25354feb51a0c2a89d7473f6decfdcc1097
ddpalacios <palaciosdanieldario@gmail.com>
2020-03-02 08:51:40 -0600

    List based Queue implementation

--- Stacks_Queues/Queues.py
+++ Stacks_Queues/Queues.py
@@ -25,3 +25,38 @@ class ListQueue:
         self.size -= 1
         return data
 
+
+# Queues can also be implemented using two stacks.
+# These stacks will help us to implement  a queue
+class Queue:
+    def __init__(self):
+        self.inbound_stack = []
+        self.outbound_stack = []
+
+    # Enqueue operation
+    def enqueue(self, data):
+        self.inbound_stack.append(data)
+
+    def dequeue(self):
+        # Instead of remocing elements from the inbound stack
+        # we shift our attention to another stack that is outbound stack
+        # we shall delete the elements from our queueu only through the outbound stack
+        if not self.outbound_stack: # if the outbound stack is not empty...
+            while self.inbound_stack: # Then while there are elements in the inbound stack...
+                self.outbound_stack.append(self.inbound_stack.pop()) # Pop off the elements and append in outbound stack
+        return self.outbound_stack.pop()
+
+# We are trasfering elements between two python lists
+queue = Queue()
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+# print(queue.inbound_stack) # Output: [5, 6, 7]
+queue.dequeue()
+# print(queue.inbound_stack) # Output: []
+
+# print(queue.outbound_stack) # Output: [7, 6]
+queue.dequeue()
+# print(queue.outbound_stack) # Output: [7]
+
+
