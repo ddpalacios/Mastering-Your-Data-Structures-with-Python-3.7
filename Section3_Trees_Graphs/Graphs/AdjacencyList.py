b44be970eb1d5850aefb9982a15f9739d251a553
ddpalacios <palaciosdanieldario@gmail.com>
2020-03-06 09:42:46 -0600

    Implementation of Adjacency matrix

--- Section3_Trees_Graphs/Graphs/AdjacencyList.py
+++ Section3_Trees_Graphs/Graphs/AdjacencyList.py
@@ -29,4 +29,22 @@ graph["F"] = ['C']
 An Adjacency matrix can be implemented using the given adjacency list
 
 First, we have to obtain the key elements of the adjacency matrix. 
+The code to implement an adjacency matrix goes as follows
 """
+matrix_elements = sorted(graph.keys())
+cols = rows = len(matrix_elements) # cols and rows must be equal
+adjacency_matrix = [[0 for x in range(rows)] for y in range(cols)] # Empty matrix size NxM
+edges_list = []
+# The multi-d array is filled using a nested for loop
+for key in matrix_elements:
+    for neighbor in graph[key]:
+        edges_list.append((key, neighbor))
+print(edges_list)
+"""
+Next step is to fill it using 1 to denote the presense of an edge in the grpah. 
+"""
+for edge in edges_list:
+    index_of_first_vertex = matrix_elements.index(edge[0])
+    index_of_second_vertex = matrix_elements.index(edge[1])
+    adjacency_matrix[index_of_first_vertex][index_of_second_vertex] = 1
+
