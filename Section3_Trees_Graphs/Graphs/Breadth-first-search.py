0211366d3ce7dbb708e2146918792d2986841d03
ddpalacios <palaciosdanieldario@gmail.com>
2020-03-06 11:54:16 -0600

    Implementation to check if given two nodes, is there a connection between them?

--- Section3_Trees_Graphs/Graphs/Breadth-first-search.py
+++ Section3_Trees_Graphs/Graphs/Breadth-first-search.py
@@ -8,6 +8,7 @@ graph["B"] = ['E', 'C', 'A']
 graph["C"] = ['A', 'B', 'E', 'F']
 graph["E"] = ['B', 'C']
 graph["F"] = ['C']
+graph["Z"] = ["P"]
 
 from collections import deque
 
@@ -29,4 +30,29 @@ def breadth_first_search(graph, root):
 
     return visited_vertices
 
+
 # Time complexity of BFS is O( |V| + |E|)
+def hasRoute(node1, node2):
+    root = node1
+    visited_vertices = list()
+    graph_queue = deque([root])
+    visited_vertices.append(root)
+    node = root
+    while len(graph_queue) > 0:
+        node = graph_queue.popleft()
+        adj_nodes = graph[node]
+        if node == node1:
+            for each_node in adj_nodes:
+                current_neighbor = each_node
+                if current_neighbor == node2:
+                    return True
+        else:
+            remaining_elements = set(adj_nodes).difference(set(visited_vertices))
+            if len(remaining_elements) > 0:
+                for elem in sorted(remaining_elements):
+                    visited_vertices.append(elem)
+                    graph_queue.append(elem)
+    return False
+
+
+print(hasRoute('Z', 'P'))
